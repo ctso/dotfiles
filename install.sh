a3ce68b624abd6432f7b6cd71d857a4981e7bf25
Chris Soyars <ctsoyars@gmail.com>
2021-11-15 09:25:04 -0800

    Install bash_aliases

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# VSCode Remote Containers
-if [ -n "$REMOTE_CONTAINERS" ]; then
-    ln -sf "${SCRIPT_DIR}/bash_aliases" "${HOME}/.bash_aliases"
-fi
-
 # Common
 ln -sf "${SCRIPT_DIR}/gitconfig" "${HOME}/.gitconfig"
+ln -sf "${SCRIPT_DIR}/bash_aliases" "${HOME}/.bash_aliases"
 
 # ZSH
 ln -sf "${SCRIPT_DIR}/ohmyzsh" "${HOME}/.oh-my-zsh"
--- zshrc
+++ zshrc
@@ -3,3 +3,6 @@ ZSH_THEME="clean"
 plugins=(git)
 
 source "${HOME}/.oh-my-zsh/oh-my-zsh.sh"
+source "${HOME}/.bash_aliases"
+
+export PATH=${HOME}/.local/bin:${PATH}
